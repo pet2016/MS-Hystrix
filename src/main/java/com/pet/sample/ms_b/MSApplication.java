e7d5d2d7647f33d0bb4d01947b21a02b9d5a2810
libai <libai@mindai.com>
2017-08-22 12:01:22 +0800

    创建一个服务

---
+++ src/main/java/com/pet/sample/ms_b/MSApplication.java
@@ -0,0 +1,9 @@
+package com.pet.sample.ms_b;
+
+import org.springframework.boot.SpringApplication;
+
+public class MSApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MSApplication.class, args);
+  }
+}
