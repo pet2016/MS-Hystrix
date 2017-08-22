e6a99438aee3e68ee8b1017d9c8c8d62bcd33f0a
libai <libai@mindai.com>
2017-08-22 12:06:30 +0800

    修改服务配置

--- src/main/java/com/pet/sample/ms_b/MSApplication.java
+++ src/main/java/com/pet/sample/ms_b/MSApplication.java
@@ -1,7 +1,11 @@
 package com.pet.sample.ms_b;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@SpringBootApplication
+@EnableDiscoveryClient
 public class MSApplication {
   public static void main(String[] args) {
     SpringApplication.run(MSApplication.class, args);
