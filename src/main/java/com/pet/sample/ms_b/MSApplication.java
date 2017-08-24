0afabf19def4f99ca80a60980368b0bf15e116ba
libai <libai@mindai.com>
2017-08-24 15:18:26 +0800

    loadbalancerClient与ribbon方式调用服务

--- src/main/java/com/pet/sample/ms_b/MSApplication.java
+++ src/main/java/com/pet/sample/ms_b/MSApplication.java
@@ -3,11 +3,22 @@ package com.pet.sample.ms_b;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MSApplication {
+  
   public static void main(String[] args) {
     SpringApplication.run(MSApplication.class, args);
   }
+  
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate(){
+    return new RestTemplate();
+  }
+  
 }
